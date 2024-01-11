0a454084e6c73a6ed86795f28607ac3946a8893d
vovvkaa <ponomarenko.apple@icloud.com>
2024-01-11 19:35:53 +0200

    code 1

--- GitLesson3/ViewController.swift
+++ GitLesson3/ViewController.swift
@@ -11,8 +11,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
+    
+    
+    //Code
+    /*
+     code1
+     code1
+     code1
+     */
 
 
 }
